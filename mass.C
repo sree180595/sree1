7779a81f4673dab6487253ceda4b16aaf4d75a88
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-02 14:04:55 +0530

    Create mass.C

---
+++ mass.C
@@ -0,0 +1,12 @@
+#include<stido.h>
+void main()
+{
+char d;
+printf("enter the letter");
+scanf("%c",&d);
+if(d=='a' !! d=='e' !! d=='i' !! d=='o' !! d=='u')
+printf("it is the vowel letter");
+else
+printf("it is not the vowel letter");
+return 0;
+}
