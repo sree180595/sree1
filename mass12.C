686731456e857bbb39055fe9d372825a6b888466
sree180595 <35995020+sree180595@users.noreply.github.com>
2018-02-02 14:26:20 +0530

    Create mass12.C

---
+++ mass12.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a b c;
+clrscr();
+scan("%d%d%d,&a&b&c")
+printf("enter the numbers");
+if ((a>b)&&(a>c))
+printf("a is greater");
+else if
+printf("b is greater");
+else
+printf("c is greater");
+getch();
+return 0;
+}
